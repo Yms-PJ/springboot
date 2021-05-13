9a42118fa9b9d43d80fee7fa0b6824d64c57e4e3
yang <alstn0695@gmail.com>
2021-05-13 17:04:58 +0900

    2021/05/13 reset

---
+++ src/test/java/springboot/practice/YangApplicationTests.java
@@ -0,0 +1,13 @@
+package springboot.practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
